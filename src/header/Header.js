a8af76f4ce37ae73ebf56993d4b19e393fc454ed
Vova Chelidze <louglescom@gmail.com>
2024-04-04 01:06:25 +0300

    Added link to swagger

--- src/header/Header.js
+++ src/header/Header.js
@@ -5,13 +5,11 @@ const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Мой Сайт</Navbar.Brand>
+        <Navbar.Brand href="#home">Abz Test Web</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Главная</Nav.Link>
-            <Nav.Link href="#about">О нас</Nav.Link>
-            <Nav.Link href="#contact">Контакты</Nav.Link>
+            <Nav.Link href="https://abz-test-api-production.up.railway.app/api">Swagger</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
